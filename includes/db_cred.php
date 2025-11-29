a3a49d36580efff1fdc2228d735f06bca295a2db
monicah pirisi <monicah.lekupe@ashesi.edu.gh>
2025-11-29 05:48:12 +0000

    updates url server

--- includes/db_cred.php
+++ includes/db_cred.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * Database Credentials
- * CampusDigs Kenya - Server Configuration
- *
- * NOTE: This file only contains database credentials.
- * The getDatabaseConnection() function is defined in config.php
- */
-
 // Database server credentials
 define('SERVER', 'localhost');
 define('USERNAME', 'monicah.lekupe');
